85649c0809f0da8037e8923a082e3e859264e5a1
shiyuezhiaihy <33069359+shiyuezhiaihy@users.noreply.github.com>
2017-11-09 12:50:58 -0800

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    //res.render('index', { title: 'Express' });
-    var MongoClient = require('mongodb').MongoClient;
-    var url = "mongodb://Yue Hu:Love123456@ds251245.mlab.com:51245/yuehudb";
-
-
-    MongoClient.connect(url, function(err, db) {
-        if (err) throw err;
-
-    });
+    res.render('index', { title: 'Express' });
 });
 router.post('/', function(req, res){
     var MongoClient = require('mongodb').MongoClient;
