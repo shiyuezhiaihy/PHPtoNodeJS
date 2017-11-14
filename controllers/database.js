6e62c6c518591467b52d16f37be84b324b54b06b
shiyuezhiaihy <33069359+shiyuezhiaihy@users.noreply.github.com>
2017-11-14 15:58:37 -0800

    Add files via upload

--- controllers/database.js
+++ controllers/database.js
@@ -1,13 +1,30 @@
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://Yue1:Love123456@ds153015.mlab.com:53015/heroku_6wmwvjg8";
+var mongodb = require('mongodb');
+var mongoDBURI = process.env.MONGODB_URI || 'mongodb://Yue1:Love123456@ds153015.mlab.com:53015/heroku_6wmwvjg8';
 
 module.exports.getAllOrders =  function (request, response) {
 
-    MongoClient.connect(url, function(err, db) {
-        if (err) throw err;
-        db.collection('ORDERS').find().toArray(function(err, docs) {
-            response.render('getAllOrders', {results: docs});
-        });
-    });
+    mongodb.MongoClient.connect(mongoDBURI, function(err, db) {
+        if(err) throw err;
 
-};
+        var Routes = db.collection('Routes');
+
+        var c = Routes.find({});
+
+        c.forEach(
+            function(myDoc) {
+                console.log( "name: " + myDoc.name );  //just  loging the output to the console
+            }
+        );
+
+        Routes.find().toArray(function (err, docs) {
+            if(err) throw err;
+
+            response.render('getAllOrders', {results: docs});
+
+        });
+
+        db.close(function (err) {
+            if(err) throw err;
+        });
+    });//end of connect
+};//end function
