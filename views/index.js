92c7f7a4b7b718857ce2a7e34b06cec4abdc9520
shiyuezhiaihy <33069359+shiyuezhiaihy@users.noreply.github.com>
2017-11-14 13:53:17 -0800

    Add files via upload

---
+++ views/index.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router();
+var controllerMongoCollection = require('../controllers/database');
+/* GET home page. */
+router.get('/getAllOrders', controllerMongoCollection.getAllOrders);
+router.get('/', function(req, res, next) {
+    res.render('index', { title: 'Express'});
+});
+router.post('/', function(req, res){
+    var MongoClient = require('mongodb').MongoClient;
+<<<<<<< HEAD
+    var url = "mongodb://Yue1:Love123456@ds153015.mlab.com:53015/heroku_6wmwvjg8";
+=======
+    var url = "mongodb://<username>:<password>@ds251245.mlab.com:53015/heroku_6wmwvjg8";
+>>>>>>> f4b25ee52adbe5e01cc485685c6d5ac176f0dec3
+
+
+    MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        db.collection("ORDERS").find({name: req.param('username')}).toArray(function (err, docs) {
+            if (err) throw err;
+            docs.forEach(function (doc) {
+                res.send(doc['name'] + " " + doc['order']);
+            });
+        });
+    });
+});
+
+
+module.exports = router;
