23340cccf1d148eb84b4e6f5a0a3118e1669f5c0
shiyuezhiaihy <33069359+shiyuezhiaihy@users.noreply.github.com>
2017-11-14 15:51:46 -0800

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
+var controllerMain = require('../controllers/main');
+var controllerMongoCollection = require('../controllers/database');
 /* GET home page. */
+router.get('/getAllOrders', controllerMongoCollection.getAllOrders);
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express'});
 });
+
+
+
+    MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        db.collection("ORDERS").find({name: req.param('username')}).toArray(function (err, docs) {
+            if (err) throw err;
+            docs.forEach(function (doc) {
+                res.send(doc['name'] + " " + doc['order']);
+            });
+        });
+    });
+});
+
 
 module.exports = router;
