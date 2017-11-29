539fe3d80a0505e59a0129c5ec561b5f7f7cd337
shiyuezhiaihy <33069359+shiyuezhiaihy@users.noreply.github.com>
2017-11-28 22:30:29 -0800

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,31 @@
 var express = require('express');
 var router = express.Router();
-
-var controllerMain = require('../controllers/database');   //this will load the main controller file
-var controllerMongoCollection = require('../controllers/database'); //load controller code dealing with database mongodb and Routes collection
+var ControllerDatabase = require('../controllers/database');
 /* GET home page. */
-router.get('/getAllOrders', controllerMongoCollection.getAllOrders);
-router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express' });
+router.post('/herokuConnect', function(req, res, next) {
+    res.render('index', { title: 'Express'});
 });
+
+router.get('/', function(req, res, next) {
+    res.render('index', { title: 'Express'});
+});
+
+router.get('/mLabConnect', function(req, res, next) {
+
+    var MongoClient = require('mongodb').MongoClient;
+    var url = "mongodb://Yue1:Love123456@ds153015.mlab.com:53015/heroku_6wmwvjg8";
+
+    MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        db.collection("ORDERS").find({name: "Yue"}).toArray(function (err, docs) {
+            if (err) throw err;
+            docs.forEach(function (doc) {
+                res.send(doc['name'] + " " + doc['order']);
+            });
+        });
+    });
+});
+
+router.post('/storeData', ControllerDatabase.storeData);
 
 module.exports = router;
